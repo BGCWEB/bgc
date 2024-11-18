19357f2b3bf3d68d18fcc064084f8400911a901c
BGCWEB <BGCPHONESSS@gmail.com>
2024-11-18 10:40:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const conteudos = {
     1: `<h2>Videogames: Década de 1940-2020</h2>
         <p><strong>Primeiros Passos (Década de 1940 e 1950)</strong>: 
         1940-1950: Os primeiros conceitos de jogos eletrônicos surgiram com os experimentos em computadores e circuitos eletrônicos. O jogo "Tennis for Two", criado em 1958 por William Higinbotham, e "Spacewar!", desenvolvido em 1962 por Steve Russell e outros no MIT, são alguns dos pioneiros nesse período.
-</p>
+        </p>
+<img src="image/primeiro-game-tenis.jpg" alt="Tennis for Two" style="max-width:100%; height:auto;"/>
         <p><strong>A Era dos Fliperamas (Década de 1970)</strong>: 1972: O lançamento de "Pong" pela Atari, de Nolan Bushnell e Ted Dabney, marcou o início da popularização dos videogames. "Pong" era um jogo simples de tênis de mesa que se tornou um sucesso em fliperamas
         1978: "Space Invaders" da Taito, criado por Tomohiro Nishikado, revolucionou os fliperamas com seus gráficos pixelados e jogabilidade envolvente.
 1979: "Asteroids" da Atari introduziu um novo tipo de jogabilidade com gráficos vetoriais e uma mecânica de jogo baseada em física.
@@ -12,7 +13,8 @@ const conteudos = {
 1985: A Nintendo recuperou o mercado com o lançamento do Nintendo Entertainment System (NES) e jogos icônicos como "Super Mario Bros.", que ajudaram a revitalizar a indústria de consoles.
 <p><strong> Consolidação e Inovação (Década de 1990)</strong> 1991: A Sega lançou o Sonic the Hedgehog para o Sega Genesis, estabelecendo uma rivalidade significativa com a Nintendo e criando um marco na jogabilidade e no design de personagens.
 1994: A Sony entrou no mercado com o PlayStation, que trouxe gráficos 3D e um novo paradigma para o desenvolvimento de jogos.
-1996: O Nintendo 64 foi lançado com gráficos 3D e revolucionou os jogos com "Super Mario 64", que influenciou o design de jogos em 3D.</p> 
+1996: O Nintendo 64 foi lançado com gráficos 3D e revolucionou os jogos com "Super Mario 64", que influenciou o design de jogos em 3D.</p>
+<img src="image/play4-controles.jpg" alt="Evolução Controles" style="max-width:100%; height:auto;"/>
 <p><strong> A Revolução dos Jogos (Década de 2000)</strong> 2001: A Microsoft entrou no mercado de consoles com o Xbox, introduzindo o Xbox Live para jogos online, o que alterou a forma como os jogos multiplayer eram jogados.
 2004: O Nintendo DS foi lançado, trazendo a inovação das telas duplas e a possibilidade de jogos portáteis com novos tipos de interatividade.
 2006: A Nintendo lançou o Wii, que introduziu controles de movimento e alcançou um público mais amplo com jogos acessíveis e interativos.</p>
@@ -20,6 +22,7 @@ const conteudos = {
 2011: "The Elder Scrolls V: Skyrim" foi lançado, e é amplamente elogiado por seu mundo aberto e pela profundidade da narrativa.
 2012: O "Pokémon GO", desenvolvido pela Niantic, trouxe a realidade aumentada (AR) para um público massivo, permitindo aos jogadores explorar o mundo real para capturar Pokémon.
 2013: O lançamento do PlayStation 4 e do Xbox One trouxe gráficos de alta definição e novas funcionalidades para a indústria dos videogames.</p>
+<img src="image/a-evolucao-playstation-sony.jpg" alt="Evolução Playstation" style="max-width:100%; height:auto;"/>
 <p><strong> Tendências Recentes e Futuras (Década de 2020)</strong> 2020: A chegada do PlayStation 5 e do Xbox Series X/S trouxe avanços significativos em gráficos, desempenho e carregamento mais rápido de jogos.
 Jogos em Nuvem: Serviços como Google Stadia e Xbox Cloud Gaming estão mudando a forma como os jogos são distribuídos e jogados, permitindo a execução de jogos em diversos dispositivos através da nuvem.
 Realidade Virtual e Aumentada: Tecnologias como o Oculus Quest e o PlayStation VR estão aprimorando a experiência imersiva dos videogames, enquanto a realidade aumentada continua a evoluir com inovações em dispositivos e software. </p>
@@ -48,6 +51,7 @@ Gráficos: O jogo tinha uma tela com gráficos muito simples. A tela mostrava um
 Controle: O jogo usava dois controles analógicos (potenciômetros) para mover as raquetes. Esses controles permitiam que os jogadores ajustassem a posição das suas raquetes verticalmente. </p>
 <p><strong> Descrição do Jogo </strong> Objetivo: O objetivo de "Tennis for Two" é semelhante ao de um jogo de tênis de mesa (pingue-pongue). Os jogadores controlam raquetes e tentam rebater uma bola para o lado do oponente, com o objetivo de fazer a bola passar pela linha de fundo adversária.
 Jogabilidade: O jogo simula um jogo de tênis de mesa em um ambiente bidimensional. A física da bola, como a velocidade e a trajetória, é calculada com base no ângulo em que ela é atingida, e a gravidade afeta a trajetória da bola. </p>
+<img src="image/primeiro-game-tenis.jpg" alt="Tennis for Two" style="max-width:100%; height:auto;"/>
 <p><strong> Importância e Impacto </strong> Inovação: "Tennis for Two" é significativo por ter sido um dos primeiros jogos eletrônicos interativos, demonstrando a possibilidade de entretenimento interativo através de equipamentos eletrônicos.
 Demonstração de Tecnologia: O jogo ajudou a mostrar o potencial dos osciloscópios e outros equipamentos de tecnologia na criação de experiências interativas.
 Reconhecimento: Embora não tenha sido comercializado e tenha tido uma vida útil limitada, "Tennis for Two" é frequentemente reconhecido em estudos e discussões sobre a história dos videogames.</p>
@@ -66,11 +70,13 @@ Reconhecimento Póstumo: Em anos recentes, "Tennis for Two" tem sido reconhecido
         <p><strong>Definição de Arte</strong>: Definição de Arte: A arte é tradicionalmente definida como a expressão ou aplicação criativa da habilidade e imaginação humanas, frequentemente representada em obras que têm valor estético e emocional. Isso inclui uma ampla gama de formas, como pintura, escultura, música, dança, e teatro.
 Videogames como Arte: A definição de arte pode se expandir para incluir os videogames, uma forma de mídia interativa que combina elementos visuais, sonoros e narrativos para criar experiências imersivas e emocionantes. A discussão sobre os videogames como arte se concentra em como eles utilizam esses elementos de maneira criativa e expressiva.
 </p>
+<img src="image/ghost-of-tsushima-feature-3-scaled.jpg" alt="Ghost of Tsushima" style="max-width:100%; height:auto;"/>
         <p><strong>Argumentos a Favor</strong>: Design Visual e Estético: Muitos jogos são conhecidos por seus designs visuais impressionantes e inovadores. Jogos como The Legend of Zelda: Breath of the Wild e Journey são frequentemente elogiados por suas estéticas únicas e impactantes, que refletem intenções artísticas.
 Narrativa e Enredo: Videogames muitas vezes contam histórias complexas e profundas. Jogos como The Last of Us e Bioshock Infinite são notáveis por suas narrativas envolventes e desenvolvimento de personagens, comparáveis a obras literárias e cinematográficas.
 Música e Som: A trilha sonora e o design de som são aspectos cruciais dos videogames que contribuem para a sua experiência artística. Compositores como Nobuo Uematsu (de Final Fantasy) e Austin Wintory (de Journey) são reconhecidos por suas contribuições artísticas na música para jogos.
 Interatividade e Imersão: A capacidade dos videogames de envolver ativamente os jogadores cria uma nova forma de expressão artística. A interatividade permite que os jogadores experimentem a arte de maneiras únicas e pessoais, algo que não é possível com formas de arte mais tradicionais.
 </p>
+<img src="image/kratos.jpg" alt="Kratos" style="max-width:100%; height:auto;"/>
 <p><strong> Reconhecimento e Legitimidade </strong> Museus e Exposições: O reconhecimento dos videogames como arte é reforçado pelo fato de que eles têm sido exibidos em museus e galerias de arte. O Museum of Modern Art (MoMA) em Nova York, por exemplo, tem uma coleção permanente de jogos que são considerados exemplos de design e inovação artística.
 Premiações e Críticas: Prêmios e críticas voltados para a arte em jogos também ajudam a legitimar os videogames como uma forma de arte. Eventos como os Game Awards e os prêmios de design de jogos frequentemente destacam a importância estética e criativa dos jogos. </p>
 <p><strong>  Críticas e Desafios </strong> Comercialização e Mercado: Alguns críticos argumentam que a natureza comercial dos videogames pode diminuir sua consideração como arte. A pressão para criar produtos lucrativos pode às vezes resultar em escolhas que priorizam a rentabilidade sobre a inovação artística.
@@ -79,6 +85,7 @@ Normas Culturais e Percepções: A percepção dos videogames como arte pode var
 <p><strong> Exemplos e Casos Notáveis </strong> Journey (2012): Este jogo é amplamente aclamado por sua estética visual e narrativa minimalista, sendo frequentemente citado como um exemplo de arte interativa.
 The Legend of Zelda: Breath of the Wild (2017): Com sua vasta e rica paisagem visual, este jogo é notável por sua combinação de design artístico e mecânicas de jogo inovadoras.
 The Stanley Parable (2013): Um jogo que explora e subverte as convenções do design de jogos, oferecendo uma experiência reflexiva sobre a narrativa e a escolha.</p>
+<img src="image/red-dead-redemption-2.jpg" alt="Red dead Redemption 2" style="max-width:100%; height:auto;"/>
 <p><strong> Futuro e Tendências </strong> Expansão da Percepção: À medida que os videogames continuam a evoluir e se tornar uma parte mais significativa da cultura global, é provável que sua consideração como arte continue a crescer e se diversificar.
 Integração com Outras Formas de Arte: Jogos cada vez mais estão colaborando com outras formas de arte, como cinema e música, e explorando novas formas de expressão artística.
 </p>
@@ -93,7 +100,8 @@ Integração com Outras Formas de Arte: Jogos cada vez mais estão colaborando c
         <p><strong>Primeiros Eventos e Crescimento (Década de 1990)</strong>: 1990: O torneio "Nintendo World Championships" foi um dos primeiros eventos a reunir jogadores em uma competição de grande escala. Os participantes competiam em vários jogos da Nintendo.
 1997: A criação da "Cyberathlete Professional League" (CPL) marcou o início de uma liga profissional dedicada a eSports, com torneios regulares de jogos como "Quake".
 1999: O "World Cyber Games" foi fundado na Coreia do Sul e rapidamente se tornou um dos maiores eventos internacionais de eSports, reunindo jogadores de todo o mundo para competir em uma variedade de jogos.
-</p>´
+</p>
+<img src="image/fallen.jpg" alt="O professor" style="max-width:100%; height:auto;"/>
 <p><strong>  Consolidação e Expansão (Década de 2000) </strong> 2000: A CPL começou a oferecer grandes prêmios em dinheiro, o que ajudou a estabelecer os eSports como uma carreira profissional.
 2001: O lançamento de "StarCraft" pela Blizzard Entertainment na Coreia do Sul teve um impacto significativo, pois o jogo se tornou extremamente popular e ajudou a estabelecer o país como um centro global de eSports.
 2003: A criação do "Electronic Sports World Cup" (ESWC) proporcionou um palco para competições internacionais e solidificou a legitimidade dos eSports como uma forma competitiva e global de entretenimento.
@@ -101,13 +109,15 @@ Integração com Outras Formas de Arte: Jogos cada vez mais estão colaborando c
 <p><strong> Popularização e Profissionalização (Década de 2010) </strong> 2011: A Riot Games lançou "League of Legends", que rapidamente se tornou um dos jogos de eSports mais populares do mundo. O torneio anual "League of Legends World Championship" atraiu milhões de espectadores.
 2013: O "The International" (TI) de "Dota 2" da Valve ofereceu um prêmio de mais de $2 milhões, destacando o potencial dos eSports para grandes prêmios em dinheiro e atraindo atenção global.
 2014: O "ESL One" e o "Blizzard World Championship Series" (WCS) ajudaram a expandir ainda mais o alcance dos eSports, com grandes eventos em diversas regiões e jogos.
-</p>
+</p> 
+<img src="image/lol.jpg" alt="LOL" style="max-width:100%; height:auto;"/>
 <p><strong> Consolidação e Crescimento (Década de 2020) </strong> 2020: A pandemia de COVID-19 acelerou a adoção dos eSports, com eventos sendo realizados online e alcançando audiências ainda maiores. Jogos como "Valorant" e "Among Us" ganharam destaque durante este período.
 2021: A Valorant Champions Tour (VCT) e o crescimento contínuo de ligas como a Overwatch League (OWL) e a Call of Duty League (CDL) demonstraram a profissionalização e a estrutura robusta dos eSports.
 2023: A indústria de eSports continua a expandir, com grandes investimentos de empresas e mídias, como a Liga Brasileira de Free Fire (LBFF), e o crescente número de torneios de grande escala e audiências.
 </p>
 <p><strong> Impacto Cultural e Econômico </strong> Indústria Multimilionária: Os eSports se tornaram uma indústria multimilionária, com receitas provenientes de patrocínios, publicidade, venda de ingressos e merchandising. A mídia e as plataformas de streaming, como Twitch e YouTube Gaming, desempenham um papel crucial na transmissão e promoção dos eventos.
 Reconhecimento Oficial: Em alguns países, os eSports ganharam reconhecimento oficial como esportes legítimos, com apoio de federações e entidades esportivas. Em 2020, os eSports foram incluídos nos Jogos Asiáticos como um esporte demonstrativo.
+<img src="image/fifa-25.jpg" alt="FIFA" style="max-width:100%; height:auto;"/>
 </p>
 <p><strong> Tendências Futuras </strong> Integração com Tecnologia: A realidade aumentada (AR) e a realidade virtual (VR) estão começando a influenciar o desenvolvimento dos eSports, criando novas formas de experiências imersivas.
 Expansão Global: Os eSports estão se expandindo para novas regiões e mercados, com crescimento significativo na América Latina, África e Sudeste Asiático.
